7a5f27bb19fca0fd907b2396ac5721a3f2a15a8e
hoangdinhdung05 <hoangdinhdung0205@gmail.com>
2024-09-26 17:06:30 +0700

    add bai1

---
+++ bai1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class bai1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int dc = 0, dl = 0, tc = 0, tl = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] % 2 == 0){
+                dc++;
+                tc += arr[i];
+            }
+            else{
+                dl++;
+                tl += arr[i];
+            }
+        }
+
+        System.out.println(dc);
+        System.out.println(dl);
+        System.out.println(tc);
+        System.out.println(tl);
+    }
+}
